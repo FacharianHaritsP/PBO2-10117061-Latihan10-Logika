0b22820900f554e12b873d279f5fd920380915d3
FacharianHaritsP <43494603+FacharianHaritsP@users.noreply.github.com>
2018-09-22 19:13:59 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117061/latihan10/logika/PBO210117061Latihan10Logika.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117061.latihan10.logika;
+
+/**
+ *
+ * @author Fachrian Harits Pratama
+ * Nama                 : Fachrian Harits Pratama
+ * Kelas                : PBO2
+ * NIM                  : 10117061
+ * Deskripsi program    : Program ini berisi program  menampilkan  tipe data
+ *                        logika
+ */
+public class PBO210117061Latihan10Logika {
+
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = ( umur>=17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
